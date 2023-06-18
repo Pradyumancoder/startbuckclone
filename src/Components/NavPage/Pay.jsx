94a35f20067a24364af6c15cf5b4890f3c14d66c
Pradyumancoder <shuklapradyuman786@gmail.com>
2023-06-18 19:07:40 +0530

    making-ui-for-dynamic-design-

--- src/Components/NavPage/Pay.jsx
+++ src/Components/NavPage/Pay.jsx
@@ -62,8 +62,13 @@ function Pay() {
 
         </div>
         <div className='h-[280px] w-[100%] border border-red-700 rounded-tr-3xl rounded-tl-3xl mt-[-16px] bg-white'>
-            <div className='h-[50px] w-[80%] border border-black m-auto mt-4'>
+            <div className='h-[50px] w-[60%] border border-black m-auto mt-4 rounded-l-3xl rounded-r-3xl '>
+              <div className='h-[35px] w-[80%] m-auto flex justify-between mt-2'>
+               <button className='h-[30px] w-[120px] border border-black bg-[#00754A] rounded-l-3xl rounded-r-3xl  text-white'>Pay at Store</button>
+               <button className='h-[30px] w-[120px] border border-black bg-[#00754A] rounded-l-3xl rounded-r-3xl  text-white'>Load Card</button>
+                <button className='h-[30px] w-[120px] border border-black bg-[#00754A] rounded-l-3xl rounded-r-3xl  text-white'>Past Transaction</button>
 
+              </div>
             </div>
         </div>
 
